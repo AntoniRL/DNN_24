b5dc2786fe756d9fbfa5d60b755ce97adbc01629
Antoni Radomiński-Lasek <92340230+AntoniRL@users.noreply.github.com>
2025-01-12 22:49:12 +0100

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -2,5 +2,8 @@
 
 conda install python=3.11
 conda install scikit-learn
-pip install tensorflow
+conda install tensorflow
 conda install wfdb	
+conda install numpy
+conda install keras
+conda install scipy==1.10.1
